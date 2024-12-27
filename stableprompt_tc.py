d2e22e231e29b034b3b35014cb8308ff7e8878c6
Junwon Ko <72448556+koooooooook@users.noreply.github.com>
2024-12-27 21:01:56 +0900

    Update stableprompt_tc.py

--- stableprompt_tc.py
+++ stableprompt_tc.py
@@ -13,6 +13,8 @@ import utils
 from dataset_utils import load_all_dataset,dataset_dicts,load_qa_dataset,qa_dicts,load_generation_dataset
 from peft import LoraConfig
 from datasets import Dataset
+
+
 def parser_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--target_model',type=str,default='google/gemma-1.1-7b-it')
