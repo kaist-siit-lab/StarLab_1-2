d20d0498c179fa80c59fcad1b5e3d0fe46040e6b
Junwon Ko <72448556+koooooooook@users.noreply.github.com>
2024-12-27 20:54:12 +0900

    Update stableprompt_bbii_tc.py

--- stableprompt_bbii_tc.py
+++ stableprompt_bbii_tc.py
@@ -12,6 +12,8 @@ import heapq
 import utils
 from dataset_utils import load_all_dataset,dataset_dicts,load_qa_dataset,qa_dicts,load_generation_dataset,load_bigbench
 from peft import LoraConfig
+
+#
 def parser_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--target_model',type=str,default='google/gemma-1.1-7b-it')
